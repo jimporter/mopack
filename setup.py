4d5981c20f2b09f38444727c8a589cf37fbd7845
Jim Porter <itsjimporter@gmail.com>
2023-01-12 20:35:46 -0800

    Move coverage configuration to setup.cfg

--- setup.py
+++ setup.py
@@ -22,32 +22,16 @@ class Coverage(Command):
     def finalize_options(self):
         pass
 
-    def _make_subproc_rc(self):
-        # For reasons I don't fully understand, coverage.py doesn't correctly
-        # cover files in integration tests using our normal `.coveragerc`. To
-        # fix this, change that line to `include = ${TOP}/mopack/*` so we get
-        # full coverage. We don't do this universally, since using `source`
-        # makes sure that if we run a subset of tests, coverage.py picks up
-        # files with 0 coverage.
-        with open(os.path.join(root_dir, '.coveragerc')) as f:
-            rc = f.read()
-        fixed = rc.replace('source = mopack', 'include = ${TOP}/mopack/*')
-
-        result = os.path.join(root_dir, '.coveragerc-subproc')
-        with open(result, 'w') as f:
-            f.write(fixed)
-        return result
-
     def run(self):
         env = dict(os.environ)
         pythonpath = os.path.join(root_dir, 'test', 'scripts')
         if env.get('PYTHONPATH'):
             pythonpath += os.pathsep + env['PYTHONPATH']
         env.update({
-            'TOP': root_dir,
+            'TOP_SRCDIR': root_dir,
             'PYTHONPATH': pythonpath,
             'COVERAGE_FILE': os.path.join(root_dir, '.coverage'),
-            'COVERAGE_PROCESS_START': self._make_subproc_rc(),
+            'COVERAGE_PROCESS_START': os.path.join(root_dir, 'setup.cfg'),
         })
 
         subprocess.run(['coverage', 'erase'], check=True)
