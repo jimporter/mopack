80257bb8501d6fc50912bdfb59305e507218e91e
Jim Porter <itsjimporter@gmail.com>
2025-05-31 19:28:17 -0700

    Drop Python 3.7 support

--- setup.py
+++ setup.py
@@ -140,7 +140,6 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
